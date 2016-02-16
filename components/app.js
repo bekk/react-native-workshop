d95c00b05d2f8aa4ec5ef0e30f7ee0e90a5c4e51
Simen Johannessen <simenlomaas@gmail.com>
2016-02-16 18:33:24 +0100

    meeeerge with master

--- components/app.js
+++ components/app.js
@@ -1,7 +1,12 @@
 'use strict';
+
 import React, {
   AppRegistry,
   Component,
+  StyleSheet,
+  Text,
+  View,
+  Navigator
 } from 'react-native';
 
 import { createStore, applyMiddleware, combineReducers } from 'redux';
@@ -11,18 +16,22 @@ import thunk from 'redux-thunk';
 import rnWorkshop from './reducers';
 const store = createStore(rnWorkshop);
 
-import MessageInput from './message-input'
+import MessageInput from './message-input';
+import ListMessageContainer from './messages-list-container';
+
+function _renderScene(route, navigator) {
+  const Component = route.component;
+  return <Component {...this.props} navigator={navigator} />
+}
 
 class App extends Component {
-
-  constructor() {
-    super();
-  }
-
   render() {
     return (
       <Provider store={store}>
-        <MessageInput />
+        <Navigator
+          initialRoute={{ component: ListMessageContainer }}
+          renderScene={_renderScene}
+        />
       </Provider>
     );
   }
