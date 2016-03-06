a8e4e2b07806bfd011d13b09b040b20cd9a5b85f
Nicklas Utgaard <nutgaard@gmail.com>
2016-03-06 15:30:47 +0100

    La inn avhengighet for å styre statusbar farge

--- android/app/src/main/java/com/reactnativeworkshop/MainActivity.java
+++ android/app/src/main/java/com/reactnativeworkshop/MainActivity.java
@@ -4,6 +4,7 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.github.xinthink.rnmk.ReactMaterialKitPackage;
+import me.neo.react.StatusBarPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -36,7 +37,8 @@ public class MainActivity extends ReactActivity {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
-        new ReactMaterialKitPackage()
+        new ReactMaterialKitPackage(),
+        new StatusBarPackage(this)
       );
     }
 }
