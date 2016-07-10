05cb8edc098e111f83cdec9c5dcc7cf5454ef067
Hindenes <aleksander.hindenes@gmail.com>
2016-07-10 13:02:57 +0200

    Upgrade RN to 29 (NB: You may need to upgrade some packages in the Android SDK Manager to make it to run on Android)
    I got a problem with a support library dependency, which i solved by installing all upgrades in the Android SDK Manager

--- android/app/src/main/java/com/reactnativeworkshop/MainActivity.java
+++ android/app/src/main/java/com/reactnativeworkshop/MainActivity.java
@@ -1,10 +1,6 @@
 package com.reactnativeworkshop;
 
 import com.facebook.react.ReactActivity;
-import com.github.xinthink.rnmk.ReactMaterialKitPackage;
-import com.imagepicker.ImagePickerPackage;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,27 +14,5 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "ReactNativeWorkshop";
-    }
-
-    /**
-     * Returns whether dev mode should be enabled.
-     * This enables e.g. the dev menu.
-     */
-    @Override
-    protected boolean getUseDeveloperSupport() {
-        return BuildConfig.DEBUG;
-    }
-
-    /**
-     * A list of packages used by the app. If the app uses additional views
-     * or modules besides the default ones, add more packages here.
-     */
-    @Override
-    protected List<ReactPackage> getPackages() {
-        return Arrays.<ReactPackage>asList(
-            new MainReactPackage(),
-        new ReactMaterialKitPackage(),
-        new ImagePickerPackage()
-        );
     }
 }
