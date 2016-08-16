9596127b243643ccbb15c4c6601cf95201c3349f
Markus Nilssen <markus.nilssen@bekk.no>
2016-08-16 17:02:17 +0200

    Bump to react native .31

--- android/app/src/main/java/com/reactnativeworkshop/MainApplication.java
+++ android/app/src/main/java/com/reactnativeworkshop/MainApplication.java
@@ -9,11 +9,6 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
-import com.github.xinthink.rnmk.ReactMaterialKitPackage;
-import com.imagepicker.ImagePickerPackage;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -28,9 +23,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new ReactMaterialKitPackage(),
-          new ImagePickerPackage()
+          new MainReactPackage()
       );
     }
   };
