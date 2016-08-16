4a13a2c513a14c48eb6be0f3ed3ce7aa77578046
Markus Nilssen <markus.nilssen@bekk.no>
2016-08-16 17:15:03 +0200

    Fix android deps

--- android/app/src/main/java/com/reactnativeworkshop/MainApplication.java
+++ android/app/src/main/java/com/reactnativeworkshop/MainApplication.java
@@ -3,6 +3,10 @@ package com.reactnativeworkshop;
 import android.app.Application;
 import android.util.Log;
 
+import com.github.xinthink.rnmk.ReactMaterialKitPackage;
+import com.imagepicker.ImagePickerPackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
+
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -23,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
+          new ReactMaterialKitPackage(),
+          new ImagePickerPackage(),
           new MainReactPackage()
       );
     }
