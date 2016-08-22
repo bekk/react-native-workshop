a69afd0252c9b700dd73fd7541dad832decc20fb
Markus Nilssen <markus.nilssen@bekk.no>
2016-08-22 19:12:08 +0200

    Solution to exercise 2

---
+++ app/components/custom-image.js
@@ -0,0 +1,41 @@
+'use strict';
+import React, { Component } from 'react';
+import { Image } from 'react-native';
+import placeholder from '../resources/default-placeholder.png';
+
+export default class CustomImage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      failedToLoadImage: false
+    };
+  }
+
+  _onLoadImageFailure() {
+    this.setState({ failedToLoadImage: true });
+  }
+
+  render() {
+    if (this.state.failedToLoadImage) {
+      return null;
+    }
+
+    return <Image
+      style={ styles.thumb }
+      source={{ uri: this.props.source }}
+      defaultSource={placeholder}
+      onError={ this._onLoadImageFailure.bind(this) } />;
+  }
+}
+
+CustomImage.propTypes = {
+  source: React.PropTypes.string.isRequired
+};
+
+const styles = {
+  thumb: {
+    marginBottom: 5,
+    flexDirection:'row',
+    height: 240
+  }
+};
--- app/components/message-list.js
+++ app/components/message-list.js
@@ -1,6 +1,7 @@
 'use strict';
 import React, { Component } from 'react';
 import { ListView, RefreshControl, View, Text } from 'react-native';
+import Message from './message';
 import { Colors } from './../config/design';
 const DSConfig = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.id !== r2.id});
 
@@ -18,8 +19,42 @@ export default class MessageList extends Component {
 
   render() {
     // Hint: React-native has a ListView component, which is great for lists *wink, wink*
-    return <View><Text>List</Text></View>;
+    let messages = this.props.messages.filter(validateMessage).reverse();
+    let dataSource = this._getDataSource(messages);
+
+    return (
+      <ListView
+        style={{ flex: 1 }}
+        refreshControl={
+          <RefreshControl
+            refreshing={ this.props.refreshing }
+            onRefresh={ this.props.refreshView }
+            tintColor="#ff0000"
+            title="Laster..."
+            colors={ [Colors.Red, Colors.Green, Colors.Yellow] }
+            />
+        }
+        dataSource={ dataSource }
+        renderRow={ renderRow }
+        />
+    );
   }
+}
+
+const renderRow = (rowData) => {
+  const image = rowData.image;
+  const from = rowData.from || '';
+  const message = rowData.message || '';
+
+  return (
+    <Message from={ from }
+      message={ message }
+      image={ image } />
+  );
+}
+
+const validateMessage = message => {
+  return message.message && message.message.length > 0
 }
 
 MessageList.propTypes = {
