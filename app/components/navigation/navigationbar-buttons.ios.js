e15da2ed95cee8f5d2f45f5136775d0f0dda64a1
Hasnae Rehioui <hrehioui@atlassian.com>
2016-09-06 12:37:41 +0200

    yey added the navigate to new message button

--- app/components/navigation/navigationbar-buttons.ios.js
+++ app/components/navigation/navigationbar-buttons.ios.js
@@ -13,15 +13,33 @@ var styles = StyleSheet.create({
     justifyContent: 'center',
     top: 5,
     right: 0,
-    height: 50
+    height: 50,
+  },
+  navBarRightButtonText : {
+    color : Colors.White,
+    padding: 10,
+    fontWeight: '200',
+    fontSize: 20
   }
 });
 
 const navigationBarButtons = (dispatch) => ({
-
   RightButton: function (route) {
     // Hint dispatch navigateTo(newMessage)) to change the route (and use the styles above for layout)
-    return <View />;
+    if (route === newMessageRoute) {
+      return <View/>
+    }
+
+    return (
+      <View>
+        <TouchableOpacity
+          style={styles.navRightButton}
+          onPress={() => dispatch(navigateTo(newMessageRoute)) }
+          >
+          <Text style={styles.navBarRightButtonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+    );
   }
 });
 
